a66c855c94cb48530a90136191e5d8904907684c
Sergey <104456968+Solod-S@users.noreply.github.com>
2024-05-14 17:25:36 +0300

    add greeting cronjob + content, add farewells cronjob + content
    add greeting cronjob + content, add farewells cronjob + content

--- server.js
+++ server.js
@@ -1,33 +1,91 @@
 const dotenv = require("dotenv");
 const cron = require("node-cron");
-const { isLastFridayOfMonth } = require("date-fns");
+const {
+  endOfMonth,
+  isWeekend,
+  getDay,
+  subDays,
+  isSameDay,
+} = require("date-fns");
 const {
   contactsMessageText,
   faqMessageText,
   cron1cMessageText,
+  greetings,
+  farewells,
 } = require("./constant/messages");
 const { bot } = require("./app");
 
 const cronStickerUrl = "./img/inline/1c_1.gif";
-
-const chatId = "-1002086154595";
+// product
+const chatId = "-215426713";
+// test
+// const chatId = "-1002086154595";
 
 dotenv.config();
 
-cron.schedule("0 17 * * 5", () => {
-  // Проверяем, является ли текущая дата последней пятницей месяца
-  if (isLastFridayOfMonth(new Date())) {
-    // bot.sendMessage(chatId, cron1cMessageText);
+const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+const randomFarewell = farewells[Math.floor(Math.random() * farewells.length)];
+// npm run startarmors
+// npm run stoparmors
+
+cron.schedule("0 19 * * *", () => {
+  const currentDate = new Date();
+  const lastDayOfMonth = endOfMonth(currentDate);
+
+  if (isSameDay(currentDate, lastDayOfMonth)) {
     bot.sendAnimation(chatId, cronStickerUrl, {
       caption: cron1cMessageText,
       parse_mode: "Markdown",
     });
   }
 });
-bot.sendAnimation(chatId, cronStickerUrl, {
-  caption: cron1cMessageText,
-  parse_mode: "Markdown",
+
+cron.schedule("0 18 * * *", () => {
+  const currentDate = new Date();
+  let lastWorkDayOfMonth = endOfMonth(currentDate);
+
+  // Идем от последнего дня месяца назад до тех пор, пока не найдем рабочий день
+  while (isWeekend(lastWorkDayOfMonth)) {
+    lastWorkDayOfMonth = subDays(lastWorkDayOfMonth, 1);
+  }
+
+  // Проверяем, совпадает ли последний рабочий день месяца с сегодняшним днем
+  if (isSameDay(currentDate, lastWorkDayOfMonth)) {
+    bot.sendAnimation(chatId, cronStickerUrl, {
+      caption: cron1cMessageText,
+      parse_mode: "Markdown",
+    });
+  }
 });
+
+// Расписание для отправки приветствий в будние дни в 9:00 утра (понедельник-пятница)
+cron.schedule("0 9 * * 1-5", () => {
+  bot.sendMessage(chatId, randomGreeting, { parse_mode: "Markdown" });
+});
+
+// Расписание для отправки прощаний в будние дни в 18:00 вечера (понедельник-пятница)
+cron.schedule("0 18 * * 1-5", () => {
+  bot.sendMessage(
+    chatId,
+    randomFarewell + "\n\nНе забувайте закрити 1С наприкінці робочого дня.",
+    { parse_mode: "Markdown" }
+  );
+});
+
+// bot.sendMessage(
+//   "-215426713",
+//   randomFarewell + "\n\nНе забувайте закрити 1С наприкінці робочого дня.",
+//   { parse_mode: "Markdown" }
+// );
+
+// bot.sendAnimation(chatId, cronStickerUrl, {
+//   caption: cron1cMessageText,
+//   parse_mode: "Markdown",
+// });
+
+// bot.sendMessage(chatId, randomGreeting, { parse_mode: "Markdown" });
+
 bot.setMyCommands([
   { command: "/contacts", description: "Контакти" },
   { command: "/faq", description: "Питання, що часто задаються" },
@@ -35,7 +93,7 @@ bot.setMyCommands([
 ]);
 
 bot.on("message", async msg => {
-  // console.log(`msg`, msg.chat);
+  console.log(`msg`, msg.chat);
   // слушатель событий сообщения
   const chatId = msg.chat.id;
   const text = msg.text;
